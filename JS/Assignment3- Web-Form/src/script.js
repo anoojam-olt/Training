487aafbc23ea1d2af8d10cd1d78dead93398602e
anoojam-olt <139334918+anoojam-olt@users.noreply.github.com>
2023-09-21 12:24:10 +0530

    JS Assignment3

---
+++ JS/Assignment3- Web-Form/src/script.js
@@ -0,0 +1,201 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('mainForm');
+    const formDataArray = []; // Array to store form data
+
+    // Function to add form data to the table
+    function addFormDataToTable(formData) {
+        const tableBody = document.getElementById('dataTableBody');
+        const newRow = tableBody.insertRow();
+
+        for (const key in formData) {
+            if (formData.hasOwnProperty(key)) {
+                const cell = newRow.insertCell();
+                cell.innerHTML = formData[key];
+            }
+        }
+    }
+
+    // Function to save form data
+    function saveFormData(formData) {
+        formDataArray.push(formData);
+    }
+
+    form.addEventListener('submit', function (event) {
+        let valid = true;
+
+        function showError(elementId, errorMessage) {
+            const errorElement = document.getElementById(elementId);
+            errorElement.textContent = errorMessage;
+        }
+
+        function clearError(elementId) {
+            const errorElement = document.getElementById(elementId);
+            errorElement.textContent = '';
+        }
+
+        function validateText(element, minLength, maxLength, regexPattern, errorElementId, errorMessage) {
+            const value = element.value.trim();
+            if (value === '') {
+                showError(errorElementId, 'This field is required.');
+                valid = false;
+            } else if (value.length < minLength) {
+                showError(errorElementId, `Minimum length is ${minLength}.`);
+                valid = false;
+            } else if (value.length > maxLength) {
+                showError(errorElementId, `Maximum length is ${maxLength}.`);
+                valid = false;
+            } else if (!regexPattern.test(value)) {
+                showError(errorElementId, errorMessage);
+                valid = false;
+            } else {
+                clearError(errorElementId);
+            }
+        }
+        
+        function validateDob(element, errorElementId) {
+            const dob = element.value.trim();
+            if (dob === '' || !isValidDateFormat(dob)) {
+                showError(errorElementId, 'Invalid date of birth (yyyy-mm-dd)');
+                valid = false;
+            } else {
+                const [year, month, day] = dob.split('-').map(Number); // Split the date into year, month, and day
+                if (month < 1 || month > 12 || day < 1 || day > getDaysInMonth(year, month)) {
+                    showError(errorElementId, 'Invalid date of birth (mm and dd must be valid)');
+                    valid = false;
+                } else {
+                    const age = calculateAge(dob);
+                    if (age < 18 || age > 100) {
+                        showError(errorElementId, 'Age must be between 18 and 100');
+                        valid = false;
+                    } else {
+                        clearError(errorElementId);
+                    }
+                }
+            }
+        }
+        // Function to get the number of days in a month for a given year and month
+        function getDaysInMonth(year, month) {
+            return new Date(year, month, 0).getDate();
+        }
+        // Function to check if the date is in yyyy-mm-dd format
+        function isValidDateFormat(date) {
+            const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+            return dateRegex.test(date);
+        }
+
+        // Function to calculate age from date of birth
+        function calculateAge(dob) {
+            const dobDate = new Date(dob);
+            const currentDate = new Date();
+            const age = currentDate.getFullYear() - dobDate.getFullYear();
+            if (currentDate.getMonth() < dobDate.getMonth() || (currentDate.getMonth() === dobDate.getMonth() && currentDate.getDate() < dobDate.getDate())) {
+                return age - 1;
+            }
+            return age;
+        }
+
+        function showError(elementId, errorMessage) {
+            const errorElement = document.getElementById(elementId);
+            errorElement.textContent = errorMessage;
+        }
+
+        function clearError(elementId) {
+            const errorElement = document.getElementById(elementId);
+            errorElement.textContent = '';
+        }
+
+        function validateText(element, minLength, maxLength, regexPattern, errorElementId, errorMessage) {
+            const value = element.value.trim();
+            if (value === '' || value.length < minLength || value.length > maxLength || !regexPattern.test(value)) {
+                showError(errorElementId, errorMessage);
+                valid = false;
+            } else {
+                clearError(errorElementId);
+            }
+        }
+
+        function validateRadio(elementName, errorElementId, errorMessage) {
+            const radios = form.elements[elementName];
+            if (!Array.from(radios).some(radio => radio.checked)) {
+                showError(errorElementId, errorMessage);
+                valid = false;
+            } else {
+                clearError(errorElementId);
+            }
+        }
+
+        function validateCheckbox(elementName, errorElementId, errorMessage) {
+            const checkboxes = form.elements[elementName];
+            if (!Array.from(checkboxes).some(checkbox => checkbox.checked)) {
+                showError(errorElementId, errorMessage);
+                valid = false;
+            } else {
+                clearError(errorElementId);
+            }
+        }
+
+        function validateSelect(element, errorElementId, errorMessage) {
+            if (element.value === '') {
+                showError(errorElementId, errorMessage);
+                valid = false;
+            } else {
+                clearError(errorElementId);
+            }
+        }
+
+        function validateNumber(element, minLength, maxLength, errorElementId, errorMessage) {
+            const value = element.value.trim();
+            if (value === '' || value.length < minLength || value.length > maxLength || isNaN(value)) {
+                showError(errorElementId, errorMessage);
+                valid = false;
+            } else {
+                clearError(errorElementId);
+                element.value = parseFloat(value).toFixed(2); // Format as two decimal places
+            }
+        }
+
+        validateText(form.elements.fullname, 3, 20, /^[A-Za-z\s]+$/, 'firstName', 'Invalid full name. Only letters and spaces are allowed.');
+        validateRadio('gender', 'genderSelectError', 'Gender is mandatory');
+        validateDob(form.elements.dob, 'dobValue'); // Validate DOB field
+        validateText(form.elements.ssn, 7, 9, /^[0-9-]+$/, 'sscValue', 'Invalid social security number. Use only numbers and dashes.');
+        validateText(form.elements.address, 1, 100, /^[A-Za-z0-9\s,.-]+$/, 'addressValue', 'Invalid address. Special characters are not allowed.');
+        validateText(form.elements.phone, 7, 10, /^[0-9]+$/, 'phoneNumber', 'Invalid phone number. Use only numbers.');
+        validateText(form.elements.email, 1, 50, /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'emailValue', 'Invalid email address');
+        validateCheckbox('communication', 'communicationValue', 'Select at least one communication method');
+        form.elements.employeeId.value = Math.floor(Math.random() * 9) + 1;
+        validateText(form.elements.jobTitle, 3, 50, /^[A-Za-z\s]+$/, 'jobTitleSpan', 'Invalid job title');
+        validateSelect(form.elements.department, 'departmentValue', 'Select a department');
+        validateNumber(form.elements.salary, 3, 10, 'salaryValue', 'Invalid salary');
+        validateText(form.elements.hobbies, 3, 25, /^[A-Za-z,\s-]+$/, 'hobbiesValue', 'Invalid hobbies. Only letters, commas, and hyphens are allowed.');
+        validateText(form.elements.notes, 0, 100, /^[A-Za-z0-9\s,.]+$/, 'addNotesValue', 'Invalid additional notes');
+
+        // If validation is successful, save the form data
+        if (valid) {
+            const formData = {
+                'Full Name': form.elements.fullname.value.trim(),
+                Gender: form.elements.gender.value,
+                'Date of Birth': form.elements.dob.value.trim(),
+                'Social Security Number': form.elements.ssn.value.trim(),
+                Address: form.elements.address.value.trim(),
+                'Phone Number': form.elements.phone.value.trim(),
+                Email: form.elements.email.value.trim(),
+                'Preferred Communication': Array.from(form.querySelectorAll('input[name="communication"]:checked')).map(cb => cb.value).join(', '),
+                'Employee ID': form.elements.employeeId.value.trim(),
+                'Job Title': form.elements.jobTitle.value.trim(),
+                Department: form.elements.department.value.trim(),
+                Salary: form.elements.salary.value.trim(),
+                Hobbies: form.elements.hobbies.value.trim(),
+                'Additional Notes': form.elements.notes.value.trim(),
+            };
+
+            saveFormData(formData);
+            form.reset();
+            addFormDataToTable(formData);
+            event.preventDefault(); // Prevent form submission (you can remove this if you want to submit the form to a server)
+        }
+
+        if (!valid) {
+            event.preventDefault(); // Prevent form submission if there are validation errors
+        }
+    });
+});
--- JS/Web-Form/src/script.js
+++ JS/Web-Form/src/script.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.getElementById('submitButton');
     const clearButton = document.getElementById('cancelButton');
 
+    // var mandatoryFields = document.getElementById('requiredError')
 
     const nameValue = document.getElementById('fullname');
     const nameError = document.getElementById('firstName');
 
+    // var selectedGender = document.getElementsByName('gender');
     var genderInputs = document.getElementsByName("gender");
     const errorGender = document.getElementById('genderSelectError');
 
@@ -46,9 +48,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const additionalNotes = document.getElementById('notes');
     const notesError = document.getElementById('addNotesValue');
 
+    // Function to generate random employee ID (single digit between 1 and 10)
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 9) + 1; // Generate a random number between 1 and 9
+   
+    }
+
+    // Set the initial employee ID when the page loads
+    empInput.value = generateRandomNumber()
+
     submitButton.addEventListener('click', (event) => {
 
+
         const name = nameValue.value;
+        // const genderSelect = selectedGender.value;
         const dob = dobInput.value;
         const ssn = ssnInput.value;
         const address = addressField.value;
@@ -62,62 +75,148 @@ document.addEventListener('DOMContentLoaded', () => {
         const notesAdditional = additionalNotes.value;
         employeeId.value = generateRandomNumber();
 
+
+
+
+
         //Mandatory field validation
 
-        if (name == "" || genderInputs == "" || dob == "" || ssn == "" || communicationInputs == "" || address == "" || number == "" || email == "" || department == "" || job == "" || salary == "" || hobbies == "") {
-     
+        if (name == "" || (!genderChecked) || dob == "" || ssn == "" || (!communicationChecked) || address == "" || number == "" || email == "" || emp == "" || job == "" || salary == "" || hobbies == "") {
+            // mandatoryFields.textContent ='All fields are required';
             alert('All fields are required');
             event.preventDefault();
         }
 
+
         //Name validation
 
-        if (!/^[a-zA-Z\s]+$/.test(name) && name.length != 0) {
+        if (!/^[a-zA-Z\s]+$/.test(name) && name.length > 0) {
             nameError.style.display = "block";
             nameError.textContent = 'Name should contain alphabets and spaces only';
             event.preventDefault();
-        } else if (name.length < 3 && name.length != 0 || name.length > 20) {
+        }
+        else if (name.length < 3 && name.length > 0 || name.length > 20) {
             nameError.style.display = "block";
             nameError.textContent = 'Name must be between 3 and 20 characters.';
             event.preventDefault();
-        } else {
+        }
+        else {
             nameError.textContent = '';
         }
 
+
         //Date of birth validation
+
+
+        // const dob = dobInput.value;
 
         // Regular expression to match "yyyy-mm-dd" format
         const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+
+        // Check if the input matches the expected format
+        if (!datePattern.test(dob)) {
+            dobError.textContent = "Date must be in yyyy-mm-dd format";
+            return;
+        }
+
         const [year, month, day] = dob.split('-').map(Number);
         const dobDate = new Date(year, month - 1, day);
+
+        // Check if the date is valid
+        if (
+            dobDate.getDate() !== day ||
+            dobDate.getMonth() !== month - 1 ||
+            dobDate.getFullYear() !== year
+        ) {
+            dobError.textContent = "Invalid date";
+            return;
+        }
+
         const currentDate = new Date();
         const age = currentDate.getFullYear() - dobDate.getFullYear();
 
-        // Check if the input matches the expected format
-       
-        if (dob!=0){
-           if (!datePattern.test(dob) ) {
-            dobError.style.display = 'block';
-            dobError.textContent = "Date must be in yyyy-mm-dd format";
-            event.preventDefault();
-            }
-            // Check if the date is valid
-            else if( dobDate.getDate() !== day  ||
-            dobDate.getMonth() !== month - 1 ||
-            dobDate.getFullYear() !== year ){
-                console.log("hello");
-                dobError.style.display = 'block';
-                dobError.textContent = "Please Enter Correct Date  ";
-                event.preventDefault();
-                
-            }else if (dob!=0 && isNaN(age) || age < 18 || age > 100) {
-            dobError.style.display = 'block';
+        if (isNaN(age) || age < 18 || age > 100) {
             dobError.textContent = "Age must be between 18 and 100 years old";
+        } else {
+            dobError.textContent = "";
+        }
+
+
+
+
+        //Social Security Number Validation
+
+        if (!/^[0-9\-]+$/.test(ssn) && ssn.length > 0) {
+            sscValue.style.display = "block";
+            sscValue.textContent = 'Should accept only numbers and hyphens only';
             event.preventDefault();
         }
-    }
-        // Gender validation
+        else if (ssn.length < 7 && ssn.length > 0 || ssn.length > 9) {
+            sscValue.style.display = "block";
+            sscValue.textContent = 'Social Security Number must be between 7 and 9 characters.';
+            event.preventDefault();
+        }
+        else {
+            sscValue.textContent = '';
+        }
 
+
+
+        //Address validation
+
+        if (!/^[a-zA-Z0-9\s, -]+$/.test(address) && address.length > 0) {
+            addressError.style.display = "block"
+            addressError.textContent = 'Address should contain only alphanumeric, spaces, commas and hyphens only'
+            event.preventDefault();
+        } else {
+            addressError.textContent = '';
+        }
+
+
+
+        //Phone Number validation
+
+        if (number.length < 7 && number.length > 0 || number.length > 10) {
+            phoneSpan.style.display = "block"
+            phoneSpan.textContent = 'Phone number must be between 7 and 10 characters.';
+            event.preventDefault();
+        } else if (!(/^[0-9]+$/).test(number) && number.length > 0) {
+            phoneSpan.style.display = "block"
+            phoneSpan.textContent = 'Phone number must be in numbers.';
+            event.preventDefault();
+        } else {
+            phoneSpan.textContent = '';
+        }
+
+
+
+        //Email  validation
+
+        if (email.length > 50 && email.length > 0) {
+            emailSpan.style.display = "block"
+            emailSpan.textContent = 'The maximum length of email is 50 characters';
+            event.preventDefault();
+        } else if (!(/^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com)$/).test(email)
+ && email.length > 0) {
+            emailSpan.style.display = "block"
+            emailSpan.textContent = 'Entered email id not valid';
+            event.preventDefault();
+        } else {
+            emailSpan.textContent = '';
+        }
+
+
+        // Preferred communication validation
+        var communicationChecked = false;
+        for (var i = 0; i < communicationInputs.length; i++) {
+            if (communicationInputs[i].checked) {
+                communicationChecked = true;
+                break;
+            }
+        }
+
+
+        // Gender validation
         var genderChecked = false;
 
         for (var i = 0; i < genderInputs.length; i++) {
@@ -126,89 +225,18 @@ document.addEventListener('DOMContentLoaded', () => {
                 break;
             }
         }
-        if (!genderChecked) {
-            errorGender.textContent = 'Please select a Gender.';
-            event.preventDefault();
-        } else {
-            errorGender.textContent = '';
-        }
 
-        //Social Security Number Validation
 
-        if (!/^[0-9\-]+$/.test(ssn) && ssn.length != 0) {
-            sscValue.style.display = "block";
-            sscValue.textContent = 'Should accept only numbers and hyphens only';
-            event.preventDefault();
-        } else if (ssn.length < 7 && ssn.length != 0 || ssn.length > 9) {
-            sscValue.style.display = "block";
-            sscValue.textContent = 'Social Security Number must be between 7 and 9 characters.';
-            event.preventDefault();
-        } else {
-            sscValue.textContent = '';
-        }
-
-        //Address validation
-
-        if (!/^[a-zA-Z0-9\s, -]+$/.test(address) && address.length != 0) {
-            addressError.style.display = "block"
-            addressError.textContent = 'Address should contain only alphanumeric, spaces, commas and hyphens only'
-            event.preventDefault();
-        } else {
-            addressError.textContent = '';
-        }
-
-        //Phone Number validation
-
-        if (number.length < 7 && number.length != 0 || number.length > 10) {
-            phoneSpan.style.display = "block"
-            phoneSpan.textContent = 'Phone number must be between 7 and 10 characters.';
-            event.preventDefault();
-        } else if (!(/^[0-9]+$/).test(number) && number.length != 0) {
-            phoneSpan.style.display = "block"
-            phoneSpan.textContent = 'Phone number must be in numbers.';
-            event.preventDefault();
-        } else {
-            phoneSpan.textContent = '';
-        }
-
-        //Email  validation
-
-        if (email.length > 50 && email.length != 0) {
-            emailSpan.style.display = "block"
-            emailSpan.textContent = 'The maximum length of email is 50 characters';
-            event.preventDefault();
-        } else if (!(/^[a-zA-Z0-9._%+-]+@(gmail\.com|yahoo\.com)$/).test(email) &&
-            email.length != 0) {
-            emailSpan.style.display = "block"
-            emailSpan.textContent = 'Entered email id not valid';
-            event.preventDefault();
-        } else {
-            emailSpan.textContent = '';
-        }
-        
-        // Preferred communication validation
-
-        var communicationChecked = false;
-        for (var i = 0; i < communicationInputs.length; i++) {
-            if (communicationInputs[i].checked) {
-                communicationChecked = true;
-                break;
-            }
-        }
-        if (!communicationChecked) {
-            communicationError.textContent = 'Please select at least one Preferred Method of Communication.';
-            event.preventDefault();
-        } else {
-            communicationError.textContent = '';
-        }
 
         //Employee Id validation
 
         let employeeCodeCounter = 1;
 
         function generateRandomNumber() {
-            return Math.floor(Math.random() * 10) + 1; //Automatic number generation 
+            return Math.floor(Math.random() * 10) + 1;   //Automatic number generation
         }
+
+
 
         emp.disabled = true;
         if (emp.length > 2 || emp.length < 0) {
@@ -222,19 +250,23 @@ document.addEventListener('DOMContentLoaded', () => {
             employeeIdSpan.textContent = '';
         }
 
+
+
         //Job title validation
 
-        if (job.length < 3 && job.length != 0 || job.length > 50) {
-            jobTitleSpan.display.style = 'block';
+        if (job.length < 3 && job.length > 0 || job.length > 50) {
+            jobTitleSpan.style.display = 'block';
             jobTitleSpan.textContent = 'Job title must be between 3 and 50 characters.';
             event.preventDefault();
-        } else if (!/^[a-zA-Z\s]+$/.test(job) && job.length != 0) {
-            jobTitleSpan.display.style = 'block';
+        } else if (!/^[a-zA-Z\s]+$/.test(job) && job.length > 0) {
+            jobTitleSpan.style.display = 'block';
             jobTitleSpan.textContent = 'Job title should contain  alphabets and spaces only';
             event.preventDefault();
         } else {
             jobTitleSpan.textContent = '';
         }
+
+
 
         //Department validation
 
@@ -242,9 +274,11 @@ document.addEventListener('DOMContentLoaded', () => {
             departmentError.textContent = "Please select an option.";
             event.preventDefault();
         } else {
-            departmentError.textContent = "";
-
+            departmentError.textContent = ""; // Clear any previous error message
+            // Proceed with further processing or form submission
         }
+
+
 
         // Salary validation
 
@@ -253,7 +287,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!isNaN(formattedSalary)) {
             salaryInput.value = formattedSalary;
             salaryError.style.display = 'block'
-            salaryError.textContent = ""; 
+            salaryError.textContent = ""; // Clear any previous error message
         } else {
             salaryError.style.display = 'block'
             salaryError.textContent = "Please enter a valid salary.";
@@ -264,24 +298,27 @@ document.addEventListener('DOMContentLoaded', () => {
             salaryError.style.display = 'block'
             salaryError.textContent = 'Salary must be 3 to 10 digits';
             event.preventDefault();
-        } else if (!/^-?\d+(\.\d+)?$/.test(salary) && salary.length > 0) {
+        }
+        else if (!/^-?\d+(\.\d+)?$/.test(salary) && salary.length > 0) {
             salaryError.style.display = "block"
             salaryError.textContent = 'Salary number must be in numbers.';
             event.preventDefault();
-        } else {
+        }
+        else {
             salaryError.textContent = '';
         }
 
 
+
         //Hobbies validation
 
-        if (!/^[a-zA-Z,-]+$/.test(hobbies) && hobbies.length != 0) {
-            hobbySpan.style.display = 'block'
-            hobbySpan.textContent = 'Hobbies should contain alphabets with commas and hyphens only.';
-            event.preventDefault();
-        } else if (hobbies.length < 3 && hobbies.length != 0 || hobbies.length > 25) {
+        if (hobbies.length < 3 && hobbies.length > 0 || hobbies.length > 25) {
             hobbySpan.style.display = 'block'
             hobbySpan.textContent = 'Hobbies must be between 3 and 25 characters.';
+            event.preventDefault();
+        } else if (!/^[a-zA-Z,-]+$/.test(hobbies) && hobbies.length > 0) {
+            hobbySpan.style.display = 'block'
+            hobbySpan.textContent = 'Hobbies should contain alphabets with commas and hyphens only.';
             event.preventDefault();
         } else {
             hobbySpan.textContent = '';
